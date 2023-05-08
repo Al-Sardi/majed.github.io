4f79e915b13c46d65b66fdb5a32bf80822658a5c
Al-Sardi <115904220+Al-Sardi@users.noreply.github.com>
2023-05-08 21:59:50 +0200

    Pack into function

--- testing.js
+++ testing.js
@@ -11,37 +11,42 @@ startBtn.onclick = () => {
     ctwo.style.alignItems = "center";
     heading.textContent = name;
     ctwoh.textContent = `Hallo ${name}!`;
+    
+    calculator();
 }
 
-document.querySelector('#btnAddiere').onclick = () =>{
-    let X = parseFloat(document.querySelector('#num-x').value);
-    let Y = parseFloat(document.querySelector('#num-y').value);
-    let Z = document.getElementById('num-display');
-    let O = document.getElementById('operator');
-    O.textContent = "+";
-    Z.textContent = X + Y;
-}
-document.querySelector('#btnSubtrahiere').onclick = () =>{
-    let X = parseFloat(document.querySelector('#num-x').value);
-    let Y = parseFloat(document.querySelector('#num-y').value);
-    let Z = document.getElementById('num-display');
-    let O = document.getElementById('operator');
-    O.textContent = "-";
-    Z.textContent = X - Y;
-}
-document.querySelector('#btnMultipliziere').onclick = () =>{
-    let X = parseFloat(document.querySelector('#num-x').value);
-    let Y = parseFloat(document.querySelector('#num-y').value);
-    let Z = document.getElementById('num-display');
-    let O = document.getElementById('operator');
-    O.textContent = "x";
-    Z.textContent = X * Y;
-}
-document.querySelector('#btnDividiere').onclick = () =>{
-    let X = parseFloat(document.querySelector('#num-x').value);
-    let Y = parseFloat(document.querySelector('#num-y').value);
-    let Z = document.getElementById('num-display');
-    let O = document.getElementById('operator');
-    O.textContent = "/";
-    Z.textContent = X / Y;
+function calculator(){
+    document.querySelector('#btnAddiere').onclick = () =>{
+        let X = parseFloat(document.querySelector('#num-x').value);
+        let Y = parseFloat(document.querySelector('#num-y').value);
+        let Z = document.getElementById('num-display');
+        let O = document.getElementById('operator');
+        O.textContent = "+";
+        Z.textContent = X + Y;
+    }
+    document.querySelector('#btnSubtrahiere').onclick = () =>{
+        let X = parseFloat(document.querySelector('#num-x').value);
+        let Y = parseFloat(document.querySelector('#num-y').value);
+        let Z = document.getElementById('num-display');
+        let O = document.getElementById('operator');
+        O.textContent = "-";
+        Z.textContent = X - Y;
+    }
+    document.querySelector('#btnMultipliziere').onclick = () =>{
+        let X = parseFloat(document.querySelector('#num-x').value);
+        let Y = parseFloat(document.querySelector('#num-y').value);
+        let Z = document.getElementById('num-display');
+        let O = document.getElementById('operator');
+        O.textContent = "x";
+        Z.textContent = X * Y;
+    }
+    document.querySelector('#btnDividiere').onclick = () =>{
+        let X = parseFloat(document.querySelector('#num-x').value);
+        let Y = parseFloat(document.querySelector('#num-y').value);
+        let Z = document.getElementById('num-display');
+        let O = document.getElementById('operator');
+        O.textContent = "/";
+        Z.textContent = X / Y;
+    }
+    
 }
