0c059094bd75f165b4d2f50f4d2e958b5fbf2ff5
Al-Sardi <115904220+Al-Sardi@users.noreply.github.com>
2023-05-08 21:31:55 +0200

    4 Calc Operations

--- testing.js
+++ testing.js
@@ -16,9 +16,24 @@ startBtn.onclick = () => {
 document.querySelector('#btnAddiere').onclick = () =>{
     let X = parseFloat(document.querySelector('#num-x').value);
     let Y = parseFloat(document.querySelector('#num-y').value);
-    let Z = X + Y;
-    let D = document.getElementById('num-display');
-    
-    D.textContent = Z;
+    let Z = document.getElementById('num-display');
+    Z.textContent = X + Y;
 }
-
+document.querySelector('#btnSubtrahiere').onclick = () =>{
+    let X = parseFloat(document.querySelector('#num-x').value);
+    let Y = parseFloat(document.querySelector('#num-y').value);
+    let Z = document.getElementById('num-display');
+    Z.textContent = X - Y;
+}
+document.querySelector('#btnMultipliziere').onclick = () =>{
+    let X = parseFloat(document.querySelector('#num-x').value);
+    let Y = parseFloat(document.querySelector('#num-y').value);
+    let Z = document.getElementById('num-display');
+    Z.textContent = X * Y;
+}
+document.querySelector('#btnDividiere').onclick = () =>{
+    let X = parseFloat(document.querySelector('#num-x').value);
+    let Y = parseFloat(document.querySelector('#num-y').value);
+    let Z = document.getElementById('num-display');
+    Z.textContent = X / Y;
+}
