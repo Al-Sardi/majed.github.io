4078dd4059cd97b716eaff1cf9f49bb06c531424
Al-Sardi <115904220+Al-Sardi@users.noreply.github.com>
2023-05-10 17:27:25 +0200

    Rename testing to taschenrechner

---
+++ taschenrechner.js
@@ -0,0 +1,33 @@
+
+document.querySelector('#btnAddiere').onclick = () =>{
+    let X = parseFloat(document.querySelector('#num-x').value);
+    let Y = parseFloat(document.querySelector('#num-y').value);
+    let Z = document.getElementById('num-display');
+    let O = document.getElementById('operator');
+    O.textContent = "+";
+    Z.textContent = X + Y;
+}
+document.querySelector('#btnSubtrahiere').onclick = () =>{
+    let X = parseFloat(document.querySelector('#num-x').value);
+    let Y = parseFloat(document.querySelector('#num-y').value);
+    let Z = document.getElementById('num-display');
+    let O = document.getElementById('operator');
+    O.textContent = "-";
+    Z.textContent = X - Y;
+}
+document.querySelector('#btnMultipliziere').onclick = () =>{
+    let X = parseFloat(document.querySelector('#num-x').value);
+    let Y = parseFloat(document.querySelector('#num-y').value);
+    let Z = document.getElementById('num-display');
+    let O = document.getElementById('operator');
+    O.textContent = "x";
+    Z.textContent = X * Y;
+}
+document.querySelector('#btnDividiere').onclick = () =>{
+    let X = parseFloat(document.querySelector('#num-x').value);
+    let Y = parseFloat(document.querySelector('#num-y').value);
+    let Z = document.getElementById('num-display');
+    let O = document.getElementById('operator');
+    O.textContent = "/";
+    Z.textContent = X / Y;
+}
